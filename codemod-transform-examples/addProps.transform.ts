4cfb8f576f94c94402f1b68551ea880454524a03
ChandanGupta1499 <chandan.gupta@dream11.com>
2022-08-19 16:03:38 +0530

    feat(codemod_examples): prettier fix

--- codemod-transform-examples/addProps.transform.ts
+++ codemod-transform-examples/addProps.transform.ts
@@ -1,28 +1,45 @@
-export const parser = "tsx";
+// export const parser = "tsx";
 
 export default function removePropsTransformer(file, api) {
   const j = api.jscodeshift;
   const root = j(file.source);
 
+  const getPropsTree = (element) => {
+    const props = ["onClick", "color"];
+    const value = [
+      `() => {
+        console.log("clicked...");
+      }`,
+      "'secondary'",
+    ];
+
+    let newProps: any = [];
+    props.forEach((propName, idx) => {
+      if (!element?.value.attributes.some((el) => el.name.name === propName)) {
+        newProps.push(
+          j.jsxAttribute(
+            j.jsxIdentifier(propName),
+            j.jsxExpressionContainer(j.identifier(value[idx]))
+          )
+        );
+      }
+    });
+
+    return newProps;
+  };
+
   root.find(j.JSXOpeningElement).forEach((element) => {
     if (element?.value?.name?.name === "Button") {
-      if (!element?.value.attributes.some((el) => el.name.name === "onClick")) {
-        const newComponent = j.jsxElement(
-          j.jsxOpeningElement(element?.value.name, [
-            ...element?.value.attributes,
-            j.jsxAttribute(
-              j.jsxIdentifier("onClick"),
-              j.jsxExpressionContainer(
-                j.identifier("() => {console.log('clicked...')}")
-              )
-            ),
-          ]),
-          element.parent.node.closingElement,
-          element.parent.node.children
-        );
+      const newComponent = j.jsxElement(
+        j.jsxOpeningElement(element?.value.name, [
+          ...element?.value.attributes,
+          ...getPropsTree(element),
+        ]),
+        element.parent.node.closingElement,
+        element.parent.node.children
+      );
 
-        j(element.parent).replaceWith(newComponent);
-      }
+      j(element.parent).replaceWith(newComponent);
     }
   });
 
